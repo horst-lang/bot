fdc231e1c82d967c16e9c8e469568ee3f06f3e74
Jimi <contact@jimi.sexy>
2021-11-23 15:48:06 +0100

    💭 Removed old comments

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -21,38 +21,5 @@ module.exports = {
                 }
             }
         }
-/*
-        if (interaction.isSelectMenu()) {
-            if (interaction.customId === 'supportSelect') {
-                if (interaction.values[0] === 'ticket') {
-                    const cmd = require('../interactions/ticket');
-                    cmd.execute(interaction);
-                }else if (interaction.values[0] === 'modList') {
-                    const cmd = require('../interactions/modHelp');
-                    cmd.execute(interaction)
-                }
-            }
-        }else if (interaction.isButton()) {
-            if (interaction.customId === 'closeTick') {
-                const cmd = require('../interactions/closeTicket');
-                cmd.execute(interaction);
-            }else if (interaction.customId === 'cancelClose' || interaction.customId === 'confirmClose') {
-                const cmd = require('../interactions/ccclose');
-                cmd.execute(interaction);
-            }else if (interaction.customId === 'openTicket') {
-                const cmd = require('../interactions/ticket');
-                cmd.execute(interaction);
-            }else if (interaction.customId === 'pinBtn' || interaction.customId === 'pubBtn') {
-                const cmd = require('../interactions/fpt');
-                cmd.execute(interaction);
-            }else if (interaction.customId === 'delBtn') {
-                const cmd = require('../interactions/delete');
-                cmd.execute(interaction);
-            }else if (interaction.customId === 'upBtn' || interaction.customId === 'downBtn' || interaction.customId === 'surQuestBtn') {
-                const cmd = require('../interactions/updown');
-                cmd.execute(interaction);
-            }
-        }
-        */
 	},
 };
